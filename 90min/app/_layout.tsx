e5a957bdf8b62a506acf50aa932aa5b8410a7fd0
Gustavo Miranda do Amaral <gustavomirandadoamaral@gmail.com>
2025-11-23 22:31:25 -0300

    toasts da aplicação

--- 90min/app/_layout.tsx
+++ 90min/app/_layout.tsx
@@ -1,17 +1,28 @@
-// app/_layout.tsx
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { Slot } from 'expo-router';
+import Toast from 'react-native-toast-message';
+import CustomToast from '../components/custom-toast';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      
       <Slot />
-      <StatusBar style="auto" />
+
+      <Toast
+        config={{
+          success: (props) => <CustomToast {...props} type="success" />,
+          error: (props) => <CustomToast {...props} type="error" />,
+        }}
+      />
+
+      <StatusBar style="light" />
+
     </ThemeProvider>
   );
 }
